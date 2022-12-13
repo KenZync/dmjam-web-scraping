c597d76202af67a977548ead76640a990ff761ba
KenZ <kenzync@pm.me>
2022-12-13 15:46:16 +0700

    add user

--- index.js
+++ index.js
@@ -20,6 +20,61 @@ app.get("/ranking", (req, res) => {
   res.sendStatus(200);
 });
 
+//Download User Ranking
+app.get("/user-ranking", (req, res) => {
+  const reqName = req.params.name;
+  const id = getID(reqName);
+  const url = "https://dpjam.net/ranking"
+  axios.get(url).then(({ data }) => {
+    const $ = cheerio.load(data);
+    const musics = [];
+    const sel = "div.table-responsive-lg > table > tbody > tr";
+    const keys = [
+      "UserID",
+      "Rank",
+      "Nickname",
+      "Tier",
+      "Clear",
+    ];
+      $(sel).each(function () {
+        let keyIndex = 0;
+        const musicDetails = {};
+        $(this)
+          .find("a")
+          .each(function (i, link) {
+            musicDetails[keys[keyIndex]] = $(link).attr("href").match(/\d+/)[0];
+            keyIndex++;
+          });
+
+        $(this)
+          .find("td")
+          .each(function () {
+            musicDetails[keys[keyIndex]] = $(this).text().trim();
+            keyIndex++;
+          });
+        musics.push(musicDetails);
+      });
+      fs.writeFileSync( "data/users.json",
+        // "data/" + id + ".json",
+        JSON.stringify(musics, null, 2),
+        {
+          encoding: "utf8",
+          flag: "w",
+        },
+        (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+
+  });
+  console.log("Successfully written ranking to file");
+
+  res.sendStatus(200);
+});
+
 //Change Username to ID
 // app.get("/user/:name", (req, res) => {
 //   const reqName = req.params.name;
